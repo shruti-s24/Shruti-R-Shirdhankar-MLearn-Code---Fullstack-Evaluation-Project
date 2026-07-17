46e2ef6b1cc3089185816a6f320e87cc177f718a
Shruti Shirdhankar <shrutishirdhankar24@gmail.com>
2026-07-17 13:58:41 +0530

    Fix for prod build

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -10,12 +10,17 @@ const health_routes_1 = __importDefault(require("./routes/health.routes"));
 const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
 const admin_routes_1 = __importDefault(require("./routes/admin.routes"));
 const agent_routes_1 = __importDefault(require("./routes/agent.routes"));
+const policy_routes_1 = __importDefault(require("./routes/policy.routes"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use("/api/health", health_routes_1.default);
 app.use("/api/auth", auth_routes_1.default);
 app.use("/api/admin", admin_routes_1.default);
 app.use("/api", agent_routes_1.default);
+app.use("/api", policy_routes_1.default);
 exports.default = app;
